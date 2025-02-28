44805607339a8792f300076704778f8aa8f34bf8
FranPomba <francislevisspomba@gmail.com>
2025-02-28 10:54:11 +0100

    feat escopo inicial

---
+++ main.py
@@ -0,0 +1,23 @@
+def menu():
+    menu_bancario = """
+        ================== Bemvindo ao menu Bancario =================
+        Selecione uma das opcoes
+        [1] - Deposito
+        [2] - Saque
+        [3] - Consultar Extrato
+        [0] - Sair
+        =>
+    """
+    return menu_bancario
+
+def opcao(opcao):
+    ...
+
+def deposito(valor):
+    ...
+
+def saque(valor):
+    ...
+    
+def extrato():
+    ...
